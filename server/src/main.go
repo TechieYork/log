1810977fa40e05a032bae869a972efe1c93608d6
york528 <york528@yeah.net>
2017-03-30 17:12:09 +0800

    feat:Add command line flags and docker file

--- server/src/main.go
+++ server/src/main.go
@@ -12,6 +12,9 @@ import (
 
 	log "github.com/cihub/seelog"
 	"github.com/DarkMetrix/log/server/src/sinker"
+	"flag"
+	"strings"
+	"strconv"
 )
 
 //Init log
@@ -45,12 +48,63 @@ func InitConfig(path string) *config.Config {
 		panic(err)
 	}
 
+	//Parse flag
+	log_path := flag.String("log_path", "", "The path to save all the log files, default:/tmp/dark_metrix/log/")
+	log_file_max_size := flag.Int64("log_file_max_size", 0, "The max size of one log file, default:500M(524288000), minimum 1M")
+	log_flush_duration := flag.Int("log_flush_duration", 0, "The duration to call flush() to flush the buffer to disk, default:3 seconds, minimum 1 second")
+	log_queue_size := flag.Int("log_queue_size", 0, "The size of log transform queue to buffer logs for each file, default:10000, minimum 1000")
+	broker := flag.String("broker", "", "The kafka broker list, eg:localhost:9092,localhost:9093, default is empty")
+	topic := flag.String("topic", "", "The kafka topic which the logs will be sent to, default:net_log")
+	partition := flag.String("partition", "", "The partition list to consume the logs, default is empty")
+
+	flag.Parse()
+
+	if len(*log_path) != 0 {
+		globalConfig.Sinker.LogPath = *log_path
+	}
+
+	if *log_file_max_size != 0 {
+		globalConfig.Sinker.LogFileMaxSize = *log_file_max_size
+	}
+
+	if *log_flush_duration != 0 {
+		globalConfig.Sinker.LogFlushDuration = uint32(*log_flush_duration)
+	}
+
+	if *log_queue_size != 0 {
+		globalConfig.Sinker.LogQueueSize = uint32(*log_queue_size)
+	}
+
+	if len(*broker) != 0 {
+		brokers := strings.Split(*broker, ",")
+		globalConfig.Kafka.Broker = brokers
+	}
+
+	if len(*topic) != 0 {
+		globalConfig.Kafka.Topic = *topic
+	}
+
+	if len(*partition) != 0 {
+		partitions := strings.Split(*partition, ",")
+		globalConfig.Kafka.Partitions = []int32{}
+
+		for _, value := range partitions {
+			number, err := strconv.Atoi(value)
+
+			if err != nil {
+				panic(err)
+			}
+
+			globalConfig.Kafka.Partitions = append(globalConfig.Kafka.Partitions, int32(number))
+		}
+	}
+
 	log.Info("Config:")
 
 	log.Infof("    sinker.log_path: %s", globalConfig.Sinker.LogPath)
 	log.Infof("    sinker.log_file_max_size: %d Bytes", globalConfig.Sinker.LogFileMaxSize)
 	log.Infof("    sinker.log_flush_duration: %d Seconds", globalConfig.Sinker.LogFlushDuration)
-	log.Infof("    sinker.log_flush_duration: %d Seconds", globalConfig.Sinker.LogQueueSize)
+	log.Infof("    sinker.log_queue_size: %d", globalConfig.Sinker.LogQueueSize)
 
 	log.Infof("    kafka.broker: %s", globalConfig.Kafka.Broker)
 	log.Infof("    kafka.topic: %s", globalConfig.Kafka.Topic)
